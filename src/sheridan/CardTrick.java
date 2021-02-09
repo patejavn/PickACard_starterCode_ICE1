ca1cf9fefb7bec05e9f5bcce914b13d8d8352ead
javni <javni@192.168.2.13>
2021-02-08 23:54:41 -0500

    Updated the CardTrick,java according to the requirement

--- src/sheridan/CardTrick.java
+++ src/sheridan/CardTrick.java
@@ -1,9 +1,12 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * @modifier=Javnika Patel
+ * StudentNumber: 991600462 
  */
 package sheridan;
+
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -13,21 +16,67 @@ package sheridan;
 
 public class CardTrick 
 {
-     public static void main(String[] args)
+           /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
     {
-        Card[] magicHand = new Card[7];
+        Scanner input = new Scanner(System.in);
+
+        // Create an array to hold 7 cards
+        Card[] hand = new Card[7];
         
-        for (int i=0; i<magicHand.length; i++)
+        //System.out.println("Hand Size: " + hand.length);
+
+        // We'll use Random to generate random numbers
+        Random random = new Random();
+
+        for (int i = 0; i < hand.length; i++) 
         {
-            Card c = new Card();
+            int value = random.nextInt(13) + 1;
+            String suit = Card.SUITS[random.nextInt(4)];
+
+            Card card = new Card();
+            hand[i] = card;
             
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            hand[i].setSuit(suit);
+            hand[i].setValue(value);           
+            
         }
+
+        // print them out for debugging purposes
+        System.out.println("Here are the cards in the hand");
+        for (Card card : hand) {
+            System.out.printf("%d of %s\n", card.getValue(), card.getSuit());
+        }
+
+        // Now ask the user for a card
+        System.out.println("Pick a suit for your card");
+        for (int i = 0; i < Card.SUITS.length; i++) {
+            System.out.println((i + 1) + ": " + Card.SUITS[i]);
+        }
+        String suit = input.next();
+
+        System.out.println("Enter a value (1 to 13)");
+        int value = input.nextInt();
+
+        //Card userGuess = new Card(value, Card.SUITS[suit - 1]);
+        Card userGuess = new Card();
+        userGuess.setSuit(suit);
+        userGuess.setValue(value);
+
+        boolean match = false;
+        for (Card card : hand) {
+            if (card.getValue() == userGuess.getValue()
+                    && card.getSuit().equals(userGuess.getSuit())) {
+                match = true;
+                break;
+            }
+        }
+    
+        String response = match ? "Right guess": "No match";
         
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
+        System.out.println(response);
     }
     
 }
